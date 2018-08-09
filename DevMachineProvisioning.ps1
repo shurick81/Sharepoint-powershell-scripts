7ce2dd30e3209c56d742d71eb702a9b609cad3d3
Aleksandr SaPozhkov <alexander@sapozhkov.net>
2018-08-09 23:18:39 +0200

    Update DevMachineProvisioning.ps1

--- DevMachineProvisioning.ps1
+++ DevMachineProvisioning.ps1
@@ -16,10 +16,6 @@ choco install -y git
 choco install -y rdcman
 choco install -y nodejs.install
 choco install -y visualstudiocode
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest https://github.com/PowerShell/vscode-powershell/releases/download/v1.8.2/PowerShell-1.8.2.vsix -OutFile PowerShell-1.8.2.vsix
-code --install-extension PowerShell-1.8.2.vsix
-code --install-extension eamodio.gitlens
 choco install -y teamviewer
 choco install -y azure-cli
 choco install -y greenshot
@@ -27,4 +23,9 @@ choco install -y packer
 choco install -y google-backup-and-sync
 choco install -y xmind
 choco install -y vagrant
+# close the console and run in a new one:
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest https://github.com/PowerShell/vscode-powershell/releases/download/v1.8.2/PowerShell-1.8.2.vsix -OutFile PowerShell-1.8.2.vsix
+code --install-extension PowerShell-1.8.2.vsix
+code --install-extension eamodio.gitlens
 Enable-WindowsOptionalFeature -Online -FeatureName:Microsoft-Hyper-V -All
