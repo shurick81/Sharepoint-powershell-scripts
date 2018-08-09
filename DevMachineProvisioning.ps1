460f2c4f4f44fb79d92a27e0804f1a26134e2ba7
Aleksandr SaPozhkov <alexander@sapozhkov.net>
2018-08-09 22:03:20 +0200

    Update DevMachineProvisioning.ps1

--- DevMachineProvisioning.ps1
+++ DevMachineProvisioning.ps1
@@ -19,6 +19,7 @@ choco install -y visualstudiocode
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest https://github.com/PowerShell/vscode-powershell/releases/download/v1.8.2/PowerShell-1.8.2.vsix PowerShell-1.8.2.vsix
 code --install-extension PowerShell-1.8.2.vsix
+code --install-extension eamodio.gitlens
 choco install -y teamviewer
 choco install -y azure-cli
 choco install -y greenshot
